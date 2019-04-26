02c179f7f8d4d14aacc0f46dd980955b01ab8eb6
Jonas Norlinder <jonas@norlinder.nu>
2019-04-26 12:53:17 +0200

    Added mesh loader from previous assignments

--- lantern_viewer/lantern_viewer/data_types/context.h
+++ lantern_viewer/lantern_viewer/data_types/context.h
@@ -1,6 +1,25 @@
 #pragma once
 #include <GLFW/glfw3.h>
 #include "global_settings.h"
+#include <vector>
+
+// Struct for representing an indexed triangle mesh
+struct Mesh {
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec3> normals;
+	std::vector<uint32_t> indices;
+};
+
+// Struct for representing a vertex array object (VAO) created from a
+// mesh. Used for rendering.
+struct MeshVAO {
+	GLuint vao;
+	GLuint vertexVBO;
+	GLuint normalVBO;
+	GLuint indexVBO;
+	int numVertices;
+	int numIndices;
+};
 
 struct Context
 {
@@ -15,6 +34,9 @@ struct Context
 	GLuint positionVBO;
 	GLuint colorVBO;
 	GLuint defaultVAO;
+
+	Mesh mesh;
+	MeshVAO meshVAO;
 
 	double elapsed_time;
 };
--- lantern_viewer/lantern_viewer/main.cpp
+++ lantern_viewer/lantern_viewer/main.cpp
@@ -24,46 +24,18 @@
 #include <boost/algorithm/string/replace.hpp>
 
 
+// The attribute locations we will use in the vertex shader
+enum AttributeLocation {
+	POSITION = 0,
+	NORMAL = 1
+};
+
 std::string getExecPath()
 {
 	char result[MAX_PATH];
 	std::string path = std::string(result, GetModuleFileNameA(nullptr, result, MAX_PATH));
 	path = path.substr(0, path.find_last_of("\\/"));
 	return boost::replace_all_copy(path, "\\", "/");
-}
-
-void drawTriangle(GLuint program, GLuint vao)
-{
-	glUseProgram(program);
-
-	glBindVertexArray(vao);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-
-	glUseProgram(0);
-}
-
-
-GLuint createTriangleVAO()
-{
-	const GLfloat vertices[] = {
-		0.0f, 0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-	};
-
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-	return vao;
 }
 
 void displayImGui()
@@ -77,6 +49,27 @@ void displayImGui()
 	ImGui::Render();
 }
 
+void drawMesh(Context &ctx, GLuint program, const MeshVAO &meshVAO)
+{
+	glUseProgram(program);
+
+	// Define the model, view, and projection matrices here
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
+
+	// Concatenate the model, view, and projection matrices to a
+	// ModelViewProjection (MVP) matrix and pass it as a uniform
+	// variable to the shader program
+
+
+	glBindVertexArray(meshVAO.vao);
+	glDrawElements(GL_TRIANGLES, meshVAO.numIndices, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(ctx.defaultVAO);
+
+	glUseProgram(0);
+}
+
 void display(Context& ctx)
 {
 	glClearColor(
@@ -87,17 +80,66 @@ void display(Context& ctx)
 	);
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnable(GL_DEPTH_TEST); // ensures that polygons overlap correctly
+	drawMesh(ctx, ctx.shader_program, ctx.meshVAO);
+
 	displayImGui();
-	drawTriangle(ctx.shader_program, ctx.triangleVAO);
 }
 
+void loadMesh(const std::string &filename, Mesh *mesh)
+{
+	OBJMesh obj_mesh;
+	objMeshLoad(obj_mesh, filename);
+	mesh->vertices = obj_mesh.vertices;
+	mesh->normals = obj_mesh.normals;
+	mesh->indices = obj_mesh.indices;
+}
+
+void createMeshVAO(Context &ctx, const Mesh &mesh, MeshVAO *meshVAO)
+{
+	// Generates and populates a VBO for the vertices
+	glGenBuffers(1, &(meshVAO->vertexVBO));
+	glBindBuffer(GL_ARRAY_BUFFER, meshVAO->vertexVBO);
+	auto verticesNBytes = mesh.vertices.size() * sizeof(mesh.vertices[0]);
+	glBufferData(GL_ARRAY_BUFFER, verticesNBytes, mesh.vertices.data(), GL_STATIC_DRAW);
+
+	// Generates and populates a VBO for the vertex normals
+	glGenBuffers(1, &(meshVAO->normalVBO));
+	glBindBuffer(GL_ARRAY_BUFFER, meshVAO->normalVBO);
+	auto normalsNBytes = mesh.normals.size() * sizeof(mesh.normals[0]);
+	glBufferData(GL_ARRAY_BUFFER, normalsNBytes, mesh.normals.data(), GL_STATIC_DRAW);
+
+	// Generates and populates a VBO for the element indices
+	glGenBuffers(1, &(meshVAO->indexVBO));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshVAO->indexVBO);
+	auto indicesNBytes = mesh.indices.size() * sizeof(mesh.indices[0]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesNBytes, mesh.indices.data(), GL_STATIC_DRAW);
+
+	// Creates a vertex array object (VAO) for drawing the mesh
+	glGenVertexArrays(1, &(meshVAO->vao));
+	glBindVertexArray(meshVAO->vao);
+	glBindBuffer(GL_ARRAY_BUFFER, meshVAO->vertexVBO);
+	glEnableVertexAttribArray(POSITION);
+	glVertexAttribPointer(POSITION, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+	glBindBuffer(GL_ARRAY_BUFFER, meshVAO->normalVBO);
+	glEnableVertexAttribArray(NORMAL);
+	glVertexAttribPointer(NORMAL, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshVAO->indexVBO);
+	glBindVertexArray(ctx.defaultVAO); // unbinds the VAO
+
+	// Additional information required by draw calls
+	meshVAO->numVertices = mesh.vertices.size();
+	meshVAO->numIndices = mesh.indices.size();
+}
 
 void init(Context& ctx)
 {
 	ctx.shader_program = loadShaderProgram(getExecPath() + "/shaders/mesh.vert",
 	                                getExecPath() + "/shaders/mesh.frag");
 
-	ctx.triangleVAO = createTriangleVAO();
+	loadMesh(getExecPath() + "/models/bunny.obj", &ctx.mesh);
+	createMeshVAO(ctx, ctx.mesh, &ctx.meshVAO);
 }
 
 int main(void)
